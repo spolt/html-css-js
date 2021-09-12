d70a8eb94db72d212b1bf2769a155db94cbe103f
spolt <spoltone@gmail.com>
2021-09-12 04:44:24 +0300

    Finished

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 
 let index = 0;
 const totalWorkItems = $(".work-item").length;
-console.log(totalWorkItems)
+//console.log(totalWorkItems)
+
+$(window).on("load",function(){
+    $(".preloader").addClass("loaded");
+})
 
 $(document).ready(function(){
+
+    //nav toggle
+    $(".nav-toggle").click(function(){
+        $(".header .nav").slideToggle()
+    })
+    $(".header .nav a").click(function(){
+        if($(window).width() < 768){
+            $("header .nav").slideToggle();
+        }
+    })
 
     //fixed header
     $(window).scroll(function(){
